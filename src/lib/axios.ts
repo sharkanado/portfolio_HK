00d81d52e2516018c483bd093a10cf8abf12cd93
Martyna Michalska <m.michalska4126@gmail.com>
2023-08-31 16:10:56 +0200

    feat: getStaticProps data fetching

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const baseURL =
   process.env.NODE_ENV === "development"
-    ? "http://localhost:1337/api"
+    ? "http://127.0.0.1:1337/api"
     : process.env.API_URL;
 
 const api = axios.create({
