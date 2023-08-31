c85d8e1723aa09076ba3e3307e55b64a7d452992
Martyna Michalska <m.michalska4126@gmail.com>
2023-08-31 15:27:01 +0200

    feat: working version of axios call to strapi

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -2,18 +2,15 @@ import axios from "axios";
 
 export const baseURL =
   process.env.NODE_ENV === "development"
-    ? "http://localhost:8080/api"
+    ? "http://localhost:1337/api"
     : process.env.API_URL;
 
 const api = axios.create({
   baseURL,
 });
 
-// We have to put in auth header every time we make a request and we have to
-// get the latest token from the cognito auth so we use an interceptor
 api.interceptors.request.use(async (config) => {
-  config.headers.Authorization = `Bearer ${process.env.API_KEY}`;
-  console.log(`Bearer ${process.env.API_KEY}`);
+  config.headers.Authorization = `Bearer ${process.env.NEXT_PUBLIC_API_KEY}`;
   return config;
 });
 
